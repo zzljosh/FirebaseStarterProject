5db1b0824e688ae51eb74e286266cd235ef481c5
prateekk91 <prateekkhurana2006@gmail.com>
2016-02-05 18:08:59 -0800

    Removing firebase dependencies

--- app/src/main/java/com/software/helloworld/StarterApplication.java
+++ app/src/main/java/com/software/helloworld/StarterApplication.java
@@ -8,15 +8,10 @@
  */
 package com.software.helloworld;
 
-import com.firebase.client.Firebase;
-
-
 public class StarterApplication extends android.app.Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
-
-    Firebase.setAndroidContext(this);
   }
 }
